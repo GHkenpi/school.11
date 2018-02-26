faf674c80ab1555f19f28a7bd7886d6a9e4f43e0
中根健介 <ken@nakanekensukenomacbook-pro.local>
2018-02-26 21:53:46 +0900

    Initial Commit

---
+++ school.11/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  school.11
+//
+//  Created by 中根健介 on 2018/02/26.
+//  Copyright © 2018年 中根健介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
